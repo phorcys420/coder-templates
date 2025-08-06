bda6fc169d842341e9b9c19818e5a0772dedc497
Phorcys <57866459+phorcys420@users.noreply.github.com>
2025-08-06 18:38:49 +0200

    fix(docker-android): don't use sudo in the android-vm feature

--- docker-android/.devcontainer/features/android-vm/install.sh
+++ docker-android/.devcontainer/features/android-vm/install.sh
@@ -4,4 +4,4 @@
 # TODO: see how we can make this a dynamic parameter/preset thing
 ANDROID_IMAGE=${ANDROID_IMAGE:-'system-images;android-34;google_apis_playstore;x86_64'}
 
-sudo --preserve-env=PATH env sdkmanager "$ANDROID_IMAGE"
+sdkmanager "$ANDROID_IMAGE"
